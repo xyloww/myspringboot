1ecb9c729a3f40187a3d65a8e32898683b786eac
xyloww <xinyu7890@gmail.com>
2023-07-02 21:54:44 +0800

    .

--- 01-spring-boot-overview/03-actuator-demo/src/main/java/com/luv2code/springboot/okay/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/luv2code/springboot/okay/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,16 @@
 package com.luv2code.springboot.okay.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // injecting properties for coach
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${coach.team}")
+    private String team;
 
     // expose "/" endpoint
     @GetMapping("/")
@@ -15,7 +21,7 @@ public class FunRestController {
     // expose a workout endpoint
     @GetMapping("/workout")
     public String getDailyWorkout(){
-        return "workout";
+        return "workout "+coachName+" "+team;
     }
 
     // expose a workout endpoint
